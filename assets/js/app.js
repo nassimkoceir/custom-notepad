9b12557f60eaf95ef62e7b76f38d078f3cb6fcfd
Nassim Koceir <nassim.koceir@gmail.com>
2019-09-30 14:04:31 +0200

    Starting stylizing + History

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,27 +1,57 @@
 (() => {
+class Note {
+    constructor(creation, title, content) {
+        this.creation = creation;
+        this.title = title;
+		this.content = content;
+    }
+}
 
 let inputKey = document.getElementById("key");
 let inputTitle = document.getElementById("title");
 let inputContent = document.getElementById("content");
 
-function autosave(dcreation, id){
-	console.log(dcreation);
-	console.log(id);
-	console.log(inputContent.value);
+let history = document.getElementById("history");
+
+function allNotes(){
+	history.innerHTML = "";
+	for (var i = 0; i < localStorage.length; i++){
+	    let card = document.createElement("div")
+		let key = parseInt(localStorage.key(i));
+		let item = JSON.parse(localStorage.getItem(key));
+
+		card.innerHTML = ${item.title} ${item.content};
+
+		history.appendChild(p);
+	}
+}
+
+function autosave(){
+	let id = inputKey.dataset.key;
+	let dcreation = inputKey.dataset.date;
+	let title = inputTitle.value;
+	let content = inputContent.value;
+
+	let note = new Note(dcreation, title, content);
+	localStorage.setItem(id, JSON.stringify(note));
 }
 
 function idNote(){
 	let dcreation = new Date();
-	let id = Math.round(Math.random() * dcreation.getTime());
+	let id = dcreation.getTime();
 
 	inputKey.dataset.key = id;
 	inputKey.dataset.date = dcreation;
 
-	inputContent.removeEventListener("input", idNote);
+	setInterval(autosave, 1000);
 
-	setInterval(autosave(dcreation, id), 10000);
+	inputContent.removeEventListener("input", idNote);
 }
 
 inputContent.addEventListener("input", idNote);
+
+allNotes();
+
+setInterval(allNotes, 5000);
 
 })();
